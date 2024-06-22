36c516aa1ead346609b9a0a19e5f67b3a2313e88
Subham Sahu <subham@slanglabs.in>
2024-06-22 21:19:45 +0530

    [Server] Added springboot project for creating Web APIs

---
+++ server/professional-couriers/src/main/java/com/tpcindia/professional_couriers/ProfessionalCouriersApplication.java
@@ -0,0 +1,13 @@
+package com.tpcindia.professional_couriers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalCouriersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalCouriersApplication.class, args);
+	}
+
+}
---
+++ server/professional-couriers/src/test/java/com/tpcindia/professional_couriers/ProfessionalCouriersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpcindia.professional_couriers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalCouriersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
