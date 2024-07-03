e6a4a977a7b17ccc7c39fd66a886d96462155f43
Subham Sahu <subhamsahu039@gmail.com>
2024-07-04 00:04:56 +0530

    [Server] Added POST API for fetching consignment number

---
+++ server/professional-couriers/src/main/java/com/tpcindia/professional_couriers/dto/ConsignmentRequestDTO.java
@@ -0,0 +1,14 @@
+package com.tpcindia.professional_couriers.dto;
+
+public class ConsignmentRequestDTO {
+    private String firmName;
+
+    // Getters and setters
+    public String getFirmName() {
+        return firmName;
+    }
+
+    public void setFirmName(String firmName) {
+        this.firmName = firmName;
+    }
+}
