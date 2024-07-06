75925f8917f94bfba5d830dd07f1c7dcb3884703
Subham Sahu <subhamsahu039@gmail.com>
2024-07-06 12:06:52 +0530

    [Server] Added all the required configs to generate war file

--- server/professional-couriers/src/main/java/com/tpcindia/professional_couriers/ProfessionalCouriersApplication.java
+++ server/professional-couriers/src/main/java/com/tpcindia/professional_couriers/ProfessionalCouriersApplication.java
@@ -2,9 +2,16 @@ package com.tpcindia.professional_couriers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProfessionalCouriersApplication {
+public class ProfessionalCouriersApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProfessionalCouriersApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessionalCouriersApplication.class, args);
