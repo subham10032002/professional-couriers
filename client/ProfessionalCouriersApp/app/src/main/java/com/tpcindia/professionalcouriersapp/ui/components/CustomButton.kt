b8abdb6f57ccef761e774f94e5ce2c4d6b5f3b1b
Subham Sahu <subham@slanglabs.in>
2024-08-03 12:34:45 +0530

    [Client] Fixed bug related to alignment of progressbar in login button and related to mandatory fields in Credit booking page

--- client/ProfessionalCouriersApp/app/src/main/java/com/tpcindia/professionalcouriersapp/ui/components/CustomButton.kt
+++ client/ProfessionalCouriersApp/app/src/main/java/com/tpcindia/professionalcouriersapp/ui/components/CustomButton.kt
@@ -1,6 +1,8 @@
 package com.tpcindia.professionalcouriersapp.ui.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -10,11 +12,11 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
@@ -54,15 +56,24 @@ fun CustomButton(
             )
         } else null
     ) {
-        if (loginState) {
-            CircularProgressIndicator(color = Color.White)
-        } else {
-            Text(
-                text = text,
-                color = textColor,
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp
-            )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (loginState) {
+                CircularProgressIndicator(
+                    color = Color.White
+                )
+            } else {
+                Text(
+                    text = text,
+                    color = textColor,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                )
+            }
         }
+
     }
 }
