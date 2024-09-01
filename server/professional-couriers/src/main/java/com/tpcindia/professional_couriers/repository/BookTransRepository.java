f91959e863ee8aa46c33972da3c4d87dfcf4b418
Subham Sahu <subhamsahu039@gmail.com>
2024-09-01 12:41:40 +0530

    [Server] Fixed bug related to fetching consignment number

--- server/professional-couriers/src/main/java/com/tpcindia/professional_couriers/repository/BookTransRepository.java
+++ server/professional-couriers/src/main/java/com/tpcindia/professional_couriers/repository/BookTransRepository.java
@@ -1,7 +1,11 @@
 package com.tpcindia.professional_couriers.repository;
 
 import com.tpcindia.professional_couriers.model.BookTrans;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +18,9 @@ public interface BookTransRepository extends JpaRepository<BookTrans, Long> {
 
     @Query("SELECT MIN(tr.accNo) FROM BookTrans tr, BookMaster ma, BookAllot bk WHERE bk.startNo = :startNo AND ma.bookType = 'Credit' AND ma.bookNo = tr.bookNo AND tr.counter = 'No' AND tr.accNo >= bk.startNo AND tr.accNo <= bk.endNo AND bk.type = 'Auto Docket Mobile'")
     Long findMinUnusedAccNoCredit(@Param("startNo") Long startNo);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE BookTrans tr SET tr.counter = :counter WHERE tr.accNo = :accNo AND tr.accCode = :accCode")
+    void updateBookTransCounter(@Param("counter") String counter, @Param("accNo") Long accNo, @Param("accCode") String accCode);
 }
