c7bd26e606570ee6db5d816e878c2cc386d17080
Subham Sahu <subham@slanglabs.in>
2024-09-08 04:00:16 +0530

    [Client] Added client support to send pdfs in mail and minor fixes related to mandatory fields and type fix for consignmemt name

--- client/ProfessionalCouriersApp/app/src/main/java/com/tpcindia/professionalcouriersapp/viewModel/uiState/CreditBookingState.kt
+++ client/ProfessionalCouriersApp/app/src/main/java/com/tpcindia/professionalcouriersapp/viewModel/uiState/CreditBookingState.kt
@@ -13,5 +13,45 @@ data class CreditBookingState(
     val isDataSubmitted: Boolean = false,
     val consignmentNumber: String = "",
     val balanceStock: String = "",
+    val pdfAddress: ByteArray? = null,
     val masterAddressDetails: MasterAddressDetails = MasterAddressDetails()
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CreditBookingState
+
+        if (error != other.error) return false
+        if (isLoading != other.isLoading) return false
+        if (destinationOptions != other.destinationOptions) return false
+        if (weight != other.weight) return false
+        if (submitEnabled != other.submitEnabled) return false
+        if (isPdfSaved != other.isPdfSaved) return false
+        if (isDataSubmitted != other.isDataSubmitted) return false
+        if (consignmentNumber != other.consignmentNumber) return false
+        if (balanceStock != other.balanceStock) return false
+        if (pdfAddress != null) {
+            if (other.pdfAddress == null) return false
+            if (!pdfAddress.contentEquals(other.pdfAddress)) return false
+        } else if (other.pdfAddress != null) return false
+        if (masterAddressDetails != other.masterAddressDetails) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = error?.hashCode() ?: 0
+        result = 31 * result + isLoading.hashCode()
+        result = 31 * result + destinationOptions.hashCode()
+        result = 31 * result + weight.hashCode()
+        result = 31 * result + submitEnabled.hashCode()
+        result = 31 * result + isPdfSaved.hashCode()
+        result = 31 * result + isDataSubmitted.hashCode()
+        result = 31 * result + consignmentNumber.hashCode()
+        result = 31 * result + balanceStock.hashCode()
+        result = 31 * result + (pdfAddress?.contentHashCode() ?: 0)
+        result = 31 * result + masterAddressDetails.hashCode()
+        return result
+    }
+}
--- client/ProfessionalCouriersApp/app/src/main/java/com/tpcindia/professionalcouriersapp/viewModel/uiState/InfoState.kt
+++ client/ProfessionalCouriersApp/app/src/main/java/com/tpcindia/professionalcouriersapp/viewModel/uiState/InfoState.kt
@@ -8,5 +8,41 @@ data class InfoState(
     val invoiceValue: String = "",
     val invoiceNumber: String = "",
     val product: String = "",
-    val ewaybill: String = ""
-)
+    val ewaybill: String = "",
+    val pdfAddress: ByteArray? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as InfoState
+
+        if (isLoading != other.isLoading) return false
+        if (error != other.error) return false
+        if (isDataSubmitted != other.isDataSubmitted) return false
+        if (isPdfSaved != other.isPdfSaved) return false
+        if (invoiceValue != other.invoiceValue) return false
+        if (invoiceNumber != other.invoiceNumber) return false
+        if (product != other.product) return false
+        if (ewaybill != other.ewaybill) return false
+        if (pdfAddress != null) {
+            if (other.pdfAddress == null) return false
+            if (!pdfAddress.contentEquals(other.pdfAddress)) return false
+        } else if (other.pdfAddress != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = isLoading.hashCode()
+        result = 31 * result + (error?.hashCode() ?: 0)
+        result = 31 * result + isDataSubmitted.hashCode()
+        result = 31 * result + isPdfSaved.hashCode()
+        result = 31 * result + invoiceValue.hashCode()
+        result = 31 * result + invoiceNumber.hashCode()
+        result = 31 * result + product.hashCode()
+        result = 31 * result + ewaybill.hashCode()
+        result = 31 * result + (pdfAddress?.contentHashCode() ?: 0)
+        return result
+    }
+}
